1463c0d16601e0b4ced26ccec69a72ccbb6b23ac
Yash Sharma <yash4688@gmail.com>
2017-10-07 18:04:34 +0530

    basic CRUD operations for bot

---
+++ dbhelper.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+
+class DBHelper:
+    def __init__(self, dbname="info.sqlite"):
+        self.dbname = dbname
+        self.conn = sqlite3.connect(dbname)
+
+    def setup(self):
+        print ("creating table")
+        stmt = "CREATE TABLE IF NOT EXISTS items (description text, owner text)"
+        self.conn.execute(stmt)
+        self.conn.commit()
+
+    def add_item(self, item_text, owner):
+        stmt = "INSERT INTO items (description, owner) VALUES (?, ?)"
+        args = (item_text, owner)
+        self.conn.execute(stmt, args)
+        self.conn.commit()
+
+    def delete_item(self, item_text, owner):
+        stmt = "DELETE FROM items WHERE description = (?) AND owner = (?)"
+        args = (item_text, owner )
+        self.conn.execute(stmt, args)
+        self.conn.commit()
+
+    def get_items(self, owner):
+        stmt = "SELECT description FROM items WHERE owner = (?)"
+        args = (owner, )
+        return [x[0] for x in self.conn.execute(stmt, args)]
